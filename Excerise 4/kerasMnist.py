8608b91dd0bed39e5d408f399829b03698b4b1c5
nakster <g00332403@gmit.ie>
2018-11-17 20:27:01 +0000

    excerise 4

---
+++ Excerise 4/kerasMnist.py
@@ -0,0 +1,15 @@
+# conda install -c conda-forge keras 
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.optimizers import Adam
+from keras.utils import np_utils
+
+
+
+
+#links https://anaconda.org/conda-forge/kerasC
